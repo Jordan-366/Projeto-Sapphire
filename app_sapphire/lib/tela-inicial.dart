9e01161f6af2db458458fac9413945c4f47382f6
Jordan-366 <jordan.pinheiro2005@gmail.com>
2026-04-03 16:18:06 -0300

    Tela inicial - em progresso

---
+++ app_sapphire/lib/tela-inicial.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
